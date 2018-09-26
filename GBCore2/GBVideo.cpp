1af50608fa629940f17fe07d7528790f6f979fc1
Parisotto Andrea <Andrea.Parisotto@saipem.com>
2018-09-26 09:43:23 +0200

    [AP] Implement video registers structure

--- GBCore2/GBVideo.cpp
+++ GBCore2/GBVideo.cpp
@@ -2,6 +2,7 @@
 #include "GBBus.h"
 
 GBVideo::GBVideo() :
+    m_Registers(VIDEO_REG_SIZE, 0),
     m_VideoRAM(VIDEO_RAM_SIZE, 0)
 {
     Reset();
@@ -11,20 +12,27 @@ void GBVideo::Reset()
 {
     GBComponent::Reset();
     m_VideoRAM.fill(0);
+    m_Registers.fill(0);
 }
 
 void GBVideo::Tick(GBBus* bus)
 {
     //check if a read request is pending and address is in range
-    if (bus->IsReadReqPending() && IsAddressInRange(bus->GetAddress()))
+    if (bus->IsReadReqPending())
     {
-        bus->SetData(static_cast<quint8>(m_VideoRAM[bus->GetAddress() - VIDEO_ADDRESS_OFFSET]));
-        bus->ReadReqAck();
+        if (IsAddressInVideoRAM(bus->GetAddress()))
+        {
+            bus->SetData(static_cast<quint8>(m_VideoRAM[bus->GetAddress() - VIDEO_RAM_ADDRESS_OFFSET]));
+            bus->ReadReqAck();
+        }
     }
     //check if a write request is pending and address is in range
-    if (bus->IsWriteReqPending() && IsAddressInRange(bus->GetAddress()))
+    if (bus->IsWriteReqPending())
     {
-        m_VideoRAM[bus->GetAddress() - VIDEO_ADDRESS_OFFSET] = static_cast<char>(bus->GetData());
-        bus->WriteReqAck();
+        if (IsAddressInVideoRAM(bus->GetAddress()))
+        {
+            m_VideoRAM[bus->GetAddress() - VIDEO_RAM_ADDRESS_OFFSET] = static_cast<char>(bus->GetData());
+            bus->WriteReqAck();
+        }
     }
 }
--- GBCore2/GBVideo.h
+++ GBCore2/GBVideo.h
@@ -3,15 +3,24 @@
 
 #include "GBComponent.h"
 
+#define VIDEO_REG_SIZE 0x000C
+#define VIDEO_REG_ADDRESS_OFFSET 0xFF40
 #define VIDEO_RAM_SIZE 0x2000
-#define VIDEO_ADDRESS_OFFSET 0x8000
+#define VIDEO_RAM_ADDRESS_OFFSET 0x8000
+
+enum class VideoRegister
+{
+
+};
 
 class GBVideo : public GBComponent
 {
 private:
+    QByteArray m_Registers;
     QByteArray m_VideoRAM;
 
-    bool IsAddressInRange(quint16 address) { return address >= VIDEO_ADDRESS_OFFSET && address < VIDEO_ADDRESS_OFFSET + VIDEO_RAM_SIZE; }
+    bool IsAddressInVideoRAM(quint16 address) { return address >= VIDEO_RAM_ADDRESS_OFFSET && address < VIDEO_RAM_ADDRESS_OFFSET + VIDEO_RAM_SIZE; }
+    bool IsAddressInVideoReg(quint16 address) { return address >= VIDEO_REG_ADDRESS_OFFSET && address < VIDEO_REG_ADDRESS_OFFSET + VIDEO_REG_SIZE; }
 public:
     GBVideo();
     void Reset() override;
